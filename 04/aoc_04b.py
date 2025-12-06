aea1df2d182d057888c9e81ed3dc3cdd3691c52c
Paul Wilson <paul.wilson@stormharvester.com>
2025-12-06 17:19:03 +0000

    Add day 4a and 4b

---
+++ 04/aoc_04b.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+input_file = Path('input.txt')
+# input_file = Path('test.txt')
+
+with open(input_file, 'r') as f:
+    input_lines = [[char for char in line.strip()] for line in f.readlines()]
+
+
+def movable_roll(input_lines, row, col):
+    max_row = len(input_lines)
+    max_col = len(input_lines[0])
+    point_value = input_lines[row][col]
+    if point_value != '@':
+        return 0
+    start_row = (row - 1) if row > 0 else row
+    start_col = (col - 1) if col > 0 else col
+    end_row = (row + 1) if row < (max_row - 1) else row
+    end_col = (col + 1) if col < (max_col - 1) else col
+
+    conv_score = 0
+    for r in range(start_row, end_row + 1):
+        for c in range(start_col, end_col + 1):
+            if (r == row) and (c == col):
+                continue
+            conv_score += (input_lines[r][c] == '@')
+
+    return conv_score < 4
+
+
+total_removed = 0
+while True:
+    removed_this_round = 0
+    for r in range(len(input_lines)):
+        for c in range(len(input_lines[0])):
+            if movable_roll(input_lines, r, c):
+                removed_this_round += 1
+                input_lines[r][c] = '.'
+    total_removed += removed_this_round
+    if removed_this_round == 0:
+        break
+
+print(total_removed)
